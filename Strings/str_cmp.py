40730453e0494c4f39235fb661bfc0342b01ef75
TheMahek <mahekshaikh1298@gmail.com>
2025-08-14 06:18:45 -0700

    String Comparison operations

---
+++ Strings/str_cmp.py
@@ -0,0 +1,14 @@
+text = "I Am Python Programmer"
+print("Python \n"*3)
+print("Python"+"Java")
+val_A = "Am" in text
+print(val_A)
+val_B = "Am" not in text
+print(val_B)
+name = "Python"
+print(name == "Python")
+print(name != "Python")
+print(name > "Python")
+print(name < "Python")
+print(name >= "Python")
+print(name <= "Python")
