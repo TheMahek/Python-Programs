35322028c61b53da1e1e3d81e9c0bc3b908bbeb9
TheMahek <mahekshaikh1298@gmail.com>
2025-08-15 05:15:47 -0700

    List Operations

---
+++ Basic/list.py
@@ -0,0 +1,12 @@
+list=[1,2,3,4,5]
+print(list)
+list.append(6)
+print(list)
+list.insert(0,0)
+print(list)
+list.remove(3)
+print(list)
+
+my_list=[2,6,1,9,3,5,7,4,8]
+my_list.sort()
+print("THe ascending order of list is: ",my_list)
